453eaf82a1ce4081120868508fda7a5876b1d705
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-31 17:31:20 -0500

    added Stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import Form from "./components/Form";
 import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "pizza", quantity: 12, packed: true },
+  { id: 4, description: "cola", quantity: 12, packed: true },
+];
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
+  const handleAddItem = (item) => {
+    setItems([...items, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
   return (
     <>
       <Logo />
+      <Form onAddItem={handleAddItem} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <Stats />
     </>
   );
 }
---
+++ src/components/Stats.jsx
@@ -0,0 +1,9 @@
+const Stats = () => {
+  return (
+    <footer className="stats">
+      🧳 <em>You have X items on your list, and you already packed X</em>
+    </footer>
+  );
+};
+
+export default Stats;
