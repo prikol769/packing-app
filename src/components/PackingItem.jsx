4d6b1c7c6113bff865d53a6f9799b382b3585ccc
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-31 17:30:53 -0500

    added PackingList component

---
+++ src/components/PackingItem.jsx
@@ -0,0 +1,12 @@
+const PackingItem = ({ item, onDeleteItem }) => {
+  return (
+    <li>
+      <span style={{ textDecoration: item.packed ? "line-through" : "none" }}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+};
+
+export default PackingItem;
